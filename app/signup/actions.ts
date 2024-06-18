9a3c68d5a502d2e3e87fb3e9e7846363e76f2ac8
Ikenna Richard <oguejioforrichard@gmail.com>
2024-06-18 10:34:41 +0100

    Add signup page

---
+++ app/signup/actions.ts
@@ -0,0 +1,58 @@
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export type FormState = {
+  message: string;
+};
+
+export async function signup(
+  prevState: FormState,
+  formData: FormData,
+): Promise<FormState> {
+  const supabase = createClient();
+
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+    options: {
+      data: {
+        first_name: formData.get("firstname") as string,
+        last_name: formData.get("lastname") as string,
+        gender: formData.get("gender") as string,
+        phone_number: formData.get("phone_number") as string,
+      },
+    },
+  };
+
+  const { error } = await supabase.auth.signUp(data);
+
+  if (error) {
+    redirect("/login?message=Error signing up");
+  }
+
+  revalidatePath("/", "layout");
+
+  return {
+    message: "success",
+  };
+}
+
+export async function confirmSignup(email: string, formData: FormData) {
+  const supabase = createClient();
+
+  const { error } = await supabase.auth.verifyOtp({
+    email: email,
+    token: formData.get("token") as string,
+    type: "email",
+  });
+
+  if (error) {
+    redirect("/login?message=Error verifying OTP");
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/private");
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,145 @@
+"use client";
+
+import { confirmSignup, signup } from "./actions";
+import SubmitButton from "../components/SubmitButton";
+import { useRef } from "react";
+import { useFormState } from "react-dom";
+
+export default function SignupPage() {
+  const [formState, formAction] = useFormState(signup, {
+    message: "",
+  });
+  const emailRef = useRef<string>(null!);
+  const updateUserEmail = confirmSignup.bind(null, emailRef.current);
+
+  return (
+    <div className="w-full max-w-lg bg-white rounded-lg shadow-md p-5 m-auto">
+      {formState.message === "success" ? (
+        <form action={updateUserEmail}>
+          <div className="mb-4">
+            <label
+              htmlFor="token"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Enter token
+            </label>
+            <input
+              type="number"
+              id="token"
+              name="token"
+              required
+              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </div>
+          <SubmitButton text="Confirm" />
+        </form>
+      ) : (
+        <form action={formAction}>
+          <h2 className="text-2xl font-bold text-gray-800 text-center mb-6">
+            Setup your account
+          </h2>
+          <div className="flex gap-3">
+            <div className="mb-4">
+              <label
+                htmlFor="firstname"
+                className="block text-gray-700 font-medium mb-2"
+              >
+                First Name
+              </label>
+              <input
+                type="text"
+                id="firstname"
+                name="firstname"
+                required
+                className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="lastname"
+                className="block text-gray-700 font-medium mb-2"
+              >
+                Last Name
+              </label>
+              <input
+                type="text"
+                id="lastname"
+                name="lastname"
+                required
+                className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              />
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Gender
+            </label>
+            <select
+              id="gender"
+              name="gender"
+              required
+              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            >
+              <option value="">Please select your gender</option>
+              <option value="male">M</option>
+              <option value="female">F</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="phone"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Phone Number
+            </label>
+            <input
+              type="number"
+              id="phone"
+              name="phone_number"
+              required
+              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={(e) => {
+                emailRef.current = e.target.value;
+              }}
+              required
+              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </div>
+          <SubmitButton text="Continue" />
+        </form>
+      )}
+    </div>
+  );
+}
