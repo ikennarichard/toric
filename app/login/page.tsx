4c62eb60bfba6d7e10999c8dc1cc01524dbff798
Ikenna Richard <oguejioforrichard@gmail.com>
2024-06-18 10:33:26 +0100

    Redirect to private if theres a valid user

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,24 +1,68 @@
-import { login, signup } from './actions'
+import Link from "next/link";
+import { login } from "./actions";
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+import SubmitButton from "../components/SubmitButton";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    return redirect("/private");
+  }
   return (
-    <form>
-      <p>
-        <label htmlFor="email">Email:</label>
-        <input id="email" name="email" type="email" required />
-      </p>
-      <p>
-        <label htmlFor="password">Password:</label>
-        <input id="password" name="password" type="password" required />
-      </p>
-      <div>
-        <button formAction={login}>Log in</button>
-      </div>
-      <div>
-        <button formAction={signup}>
+    <div className="w-full max-w-md bg-white rounded-lg shadow-md p-6 m-auto">
+      <h2 className="text-2xl font-semibold text-gray-800 text-center mb-6">
+        Login
+      </h2>
+      <form action={login}>
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            required
+            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+          />
+        </div>
+        <div className="flex items-center justify-between mb-4">
+          <a href="#" className="text-blue-600 hover:underline">
+            Forgot Password?
+          </a>
+        </div>
+        <SubmitButton text="Login" />
+      </form>
+      <p className="text-center text-gray-600 mt-6">
+        Don&apos;t have an account?{" "}
+        <Link href="/signup" className="text-blue-600 hover:underline">
           Sign up
-        </button>
-      </div>
-    </form>
-  )
+        </Link>
+      </p>
+    </div>
+  );
 }
