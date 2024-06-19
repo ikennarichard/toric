7e99c33cbd42ec64418483e423f5ea7734068295
Ikenna Richard <oguejioforrichard@gmail.com>
2024-06-19 23:40:42 +0100

    Add header and toaster provider

---
+++ app/components/ToasterProvider.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { Toaster } from "react-hot-toast"
+
+interface ToasterProviderProps {
+  children: React.ReactNode
+}
+
+export default function ToasterProvider({children}: ToasterProviderProps) {
+  return (
+    <>
+      {children}
+      <Toaster/>
+    </>
+  )
+}
