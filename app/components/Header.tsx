a3123bbc83423f2ee1eee89be0ddd1e1f6c775e4
Ikenna Richard <oguejioforrichard@gmail.com>
2024-06-19 23:43:58 +0100

    Display tab for authenticated user

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,28 +1,21 @@
-import Link from "next/link";
 import H1 from "./H1";
-import signout from "../signout/actions";
-import SubmitButton from "./SubmitButton";
+import { createClient } from "@/utils/supabase/server";
+import Register from "./Register";
 
-export default function Header() {
+
+export default async function Header() {
+
+  const supabase = createClient();
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
   return (
-    <header className="flex flex-col gap-7">
+    <header className="flex justify-between gap-7 mt-6 px-16">
       <H1 />
-      <nav>
-        <ul className="flex flex-col text-white gap-4">
-          <li className="hover:underline">
-            <Link href="#">Home</Link>
-          </li>
-          <li className="hover:underline">
-            <Link href="#">About</Link>
-          </li>
-          <li className="hover:underline">
-            <Link href="#">Investments</Link>
-          </li>
-          <form action={signout}>
-            <SubmitButton text="Sign Out" />
-          </form>
-        </ul>
-      </nav>
+      {!user ? <Register /> :  null}
     </header>
   );
 }
